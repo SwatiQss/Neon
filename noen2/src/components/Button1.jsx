21f6742c1cf8a91763c5e3f68d28e53715d18164
Swati Tiwari <swati.tiwari@qsstechnosoft.com>
2025-02-25 15:13:45 +0530

    commited

--- noen2/src/components/Button1.jsx
+++ noen2/src/components/Button1.jsx
@@ -1,10 +1,29 @@
 import { FaBell } from "react-icons/fa";
 import "../styles/btn.scss"
 import { CiCalendar } from "react-icons/ci";
+import { use, useEffect, useState } from "react";
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"; 
+import { format } from "date-fns";
+
 const Button1=()=>{
+    const [startDate,setStartDate]=useState(null);
+    const[showCalendar,setShowCalendar]=useState(false);
+    useEffect(() => {
+        console.log(startDate);
+    }, [startDate]);
+    
+  const date=new Date(startDate);
+  const formatteDate=format(date,'yyyy/MM/dd');
+  console.log(formatteDate,"formattttttt");
+
+    //function to toggle calender visibility
+    const toggleCalendar=()=>{
+        setShowCalendar(!showCalendar)
+    }
     return(
         <>
-        <button className="btn" >
+        <button className="btn" onClick={toggleCalendar} >
             <div className="emoji">
               <CiCalendar style={{color:"#FF385C"}}/>
             </div>
@@ -14,6 +33,16 @@ const Button1=()=>{
 
 
         </button>
+        {/*  conditional rendering the date picker*/}
+        {showCalendar && (
+            <DatePicker
+            selected={startDate}
+            onChange={(date)=>setStartDate(date)}
+            dateFormat="yyyy/MM/dd"
+            inline
+            
+            />
+        )}
         
         </>
     )
